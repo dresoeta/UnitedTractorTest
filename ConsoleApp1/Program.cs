24e62ed86ef79cd4ed7c78c3c805b352284136ff
Dresoeta <dresoeta@gmail.com>
2022-05-20 20:04:42 +0700

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cari nomor loker berapa ?");
+            string nomorLoker = Console.ReadLine();
+            Console.WriteLine(nomorLoker);
+
+            int[] formulaKapasitas = { 5, 6, 7 };
+
+            int _noLoker = Int32.Parse(nomorLoker);
+
+            int lantai = 0;
+
+            for (int j = 0; j < formulaKapasitas.Length && _noLoker > 0; j++)
+            {
+                if (_noLoker > 0)
+                {
+                    int kapasitasindex = formulaKapasitas[j];
+                    _noLoker = _noLoker - kapasitasindex;
+
+                    lantai = lantai + 1;
+
+                    
+                    if (_noLoker > 0)
+                    {
+                        if (j == formulaKapasitas.Length - 1)
+                            j = -1;
+                        else
+                            continue;
+                        
+                    }
+                    else
+                        break;
+                }
+            }
+
+            Console.WriteLine("Loker nomor "+ nomorLoker + " ada di lantai " + lantai);
+
+        }
+
+    }
+}
